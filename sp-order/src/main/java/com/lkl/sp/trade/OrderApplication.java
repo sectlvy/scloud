a13cbe03c5788c6a1588d91590aab026ca11e15a
kanglelu <kanglelu@huangbaoche.com>
2017-11-25 18:17:26 +0800

    1111111111111

--- sp-order/src/main/java/com/lkl/sp/trade/OrderApplication.java
+++ sp-order/src/main/java/com/lkl/sp/trade/OrderApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.lkl.sp.db.redis.JedisTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -13,7 +16,13 @@ import org.springframework.context.annotation.ComponentScan;
 public class OrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class, args);
+    	ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(OrderApplication.class, args);
+    	JedisTemplate jedisTemplate = configurableApplicationContext.getBean(JedisTemplate.class);
+    	jedisTemplate.set("aa", "dd");
+    	
+    	String aa  = jedisTemplate.get("aa");
+    	
+    	System.out.println(aa);
     }
 }
 
