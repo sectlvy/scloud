8b53710dd9f9bd92b75c433cd6ec3a4b9314c0c0
Administrator <Administrator@EXBTPKXO68JJGP6>
2017-11-26 07:57:20 +0800

    添加eureka server

--- eureka-service/src/main/java/com/lkl/sp/eureka/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/lkl/sp/eureka/EurekaServiceApplication.java
@@ -2,10 +2,12 @@ package com.lkl.sp.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableConfigServer	
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
